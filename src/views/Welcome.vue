8bb1f4bea7a5ad6d4a54fa33afe359d7c7494251
DZHdzh692817 <1120951042@qq.com>
2021-11-15 21:32:24 +0800

    后台首页

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="welcome">
-        <h1>欢迎页面</h1>
-        <!-- <el-button>去登录</el-button> -->
-        <router-link to="/Login">去登录</router-link>
+        <div class="content">
+            <div class="sub-title">欢迎体验</div>
+            <div class="title">天心城通用后台管理系统</div>
+            <div class="desc">-Vue3.x+ElementPlus+Node+mongodb打造通用后台管理系统</div>
+        </div>
+        <div class="img">
+
+        </div>
     </div>
 </template>
 <script>
@@ -10,8 +15,36 @@ export default{
     name:'Welcome'
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .welcome {
-    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    .content {
+        position: relative;
+        bottom: 40px;
+        .sub-title {
+            font-size: 30px;
+            line-height: 42px;
+            color:#333;
+        }
+        .title {
+            font-size: 40px;
+            line-height: 62px;
+            color:#409eff;
+        }
+        .desc {
+            text-align: right;
+            font-size: 14px;
+            color:#999;
+        }
+    }
+    .img{
+        margin-left: 105px;
+        background-image: url('./../assets/images/welcome.png');
+        width: 371px;
+        height: 438px;
+    }
 }
 </style>
